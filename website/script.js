2b2717e92eca6d5392fd8f36e8e780a9c4ba9c1f
Hawinf <hawinf54@gmail.com>
2025-03-23 03:35:57 +0400

    post

---
+++ website/script.js
@@ -0,0 +1,10 @@
+// function sendMail() {
+//     var params = {
+//         from_name : document.getElementById("fullname").value,
+//         email_id : document.getElementById("email_id").value,
+//         message : document.getElementById("message").value
+//     }
+//     emailjs.send("service_xy4ae5f", "template_iwxvsqp", params).then(function (res) {
+//         alert("success!" + res.status);
+//     })
+// }
